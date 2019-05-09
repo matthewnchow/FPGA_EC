9e0b10204b946d66ba97a0266e1b111365ab9893
matthewnchow <40249298+matthewnchow@users.noreply.github.com>
2019-05-09 16:33:00 -0700

    Dedicated Edge comparison module and updated pulse generator
    Pulse generator instantiates new ed_comp for each edge to solve for value to XOR with state0

---
+++ ed_comp.v
@@ -0,0 +1,20 @@
+// signed comparator, shifts answer over by ch_ID
+
+module ed_comp(count, outer_count, x, dx, ch_ID, enable, out);
+	
+	parameter IN_BITS = 32;
+	parameter OUT_BITS = 8;
+	parameter OUT_BITS_LOG2 = 3; //need to pass down one greater than log2 if not power of two
+	
+	input signed [IN_BITS-1 : 0] count;
+	input signed [IN_BITS-1 : 0] outer_count;
+	input signed [IN_BITS-1 : 0] x;
+	input signed [IN_BITS-1 : 0] dx;
+	input 		 [OUT_BITS_LOG2 - 1 : 0] ch_ID;
+	input							  		  enable;
+	output 		 [OUT_BITS - 1 : 0] out;
+
+	wire   result = enable & (count >= (x + (dx * outer_count))); 
+	assign out = result << ch_ID;
+
+endmodule 
--- pulse_logic.v
+++ pulse_logic.v
@@ -1,65 +1,75 @@
 /* Parameterized pulse generator logic module. 
 	Takes clock, initial state, period and array of edges. 
-	Edges have following format: 
-		1 bit: On/off
+	Edges have following format (MSB to LSB): 
+		1 bit: On/off enable
 		log2(max channels) bits: Channel number 
 		Countbits: Edge value (in clk cycles)
 		Countbits: Change in edge value (in clk cycles)
-	Outputs N channel pulse wvfm. */
-module pulse_logic (
-	input 											reset,
-	input 											clk,
-	input		[COUNT_BITS - 1 : 0] 			period,
-	input 	[COUNT_BITS - 1 : 0]				outer_period,
-	input	 	[CH_MAX - 1 : 0] 					state0,
-	input 	[ED_BITS * ED_MAX - 1 : 0] 	eds,
-	output 	[CH_MAX - 1 : 0] 					state
-	);
+	Outputs N channel pulse wvfm. 
+	
+	Author  : Matthew Chow
+	Contact : matthewnchow@berkeley.edu
+	Worked in simulation as of 5/10/2019
+	*/
+	
+module pulse_logic (reset, clk, period, outer_period, state0, eds, state);
 
 	// Can be passed down to modify size of inputs and outputs
 	parameter COUNT_BITS = 32; // Must be >= 1
 	parameter CH_LOG2 = 3; // Must be >= 0
-	parameter ED_MAX = 100;
-	
+	parameter ED_MAX = 255;
+		
 	// Internals
-	localparam CH_MAX = 8'b0000_0001 << CH_LOG2; //Maximum of 128 channels
-	localparam ED_BITS = COUNT_BITS * 2 + 1;
+	localparam CH_MAX = 8'b0000_0001 << CH_LOG2; //8 for testing
+	localparam ED_BITS = COUNT_BITS * 2 + CH_LOG2 + 1; //68 bits if default parameters
+
+	// IO
+	input								reset;
+	input								clk;
+	input		signed [31 : 0] 	period;
+	input		signed [31 : 0]	outer_period;
+	input				 [7 : 0]		state0;
+	input 			[68*ED_MAX - 1 : 0] 	eds;
+	output 			[7 : 0] 		state;
 	
 	// Counter that restarts at period, with outer counter that rolls restarts at big period
-	reg signed [COUNT_BITS - 1 : 0] count; //Signed so that comparisons and math are clear later
-	reg signed [COUNT_BITS - 1 : 0] outer_count; // Signed so that comparison and math are clear later
+	reg signed [COUNT_BITS - 1 : 0] count = 0; 
+	reg signed [COUNT_BITS - 1 : 0] outer_count = 0; 
+
+	// Nested, clocked counters with synchronous reset
 	always @(posedge clk) begin
 		if (reset) begin
 			count <= 0;
 			outer_count <= 0;
 		end 
 		else begin 
-			if (count >= period) begin
+			if (count >= period - 1'b1) begin
 				count <= 0;
-				outer_count <= outer_count + 1;
+				if (outer_count >= outer_period - 1'b1) outer_count <= 0;
+				else outer_count <= outer_count + 2'sd1;
 			end 
-			else count <= count + 1;
-			if (outer_count >= outer_period) outer_count <= 0;
+			else count <= count + 2'sd1;
 		end 
 	end
 	
-	// Combinatorial logic, compare count to edge value
+	// Combinatorial logic, compare count to edge value. Uses ed_comp module for comparison of each edge
 	genvar i;
+	wire [ED_MAX * CH_MAX - 1 : 0] combo;
    generate
-		wire [CH_MAX - 1 : 0] temp; 
-		wire toggle; // On/off
-		wire ch_id; // Channel ID bits
-		wire signed [COUNT_BITS - 1 : 0] x; // Value
-		wire signed [COUNT_BITS - 1 : 0] dx; // Change in value each time
 		for (i = 0; i < ED_MAX; i = i + 1) begin : ed_logic
-			assign toggle = eds[i*ED_BITS];
-			assign ch_id = eds[i*ED_BITS + CH_LOG2 : i*ED_BITS + 1];
-			assign x = eds[i*ED_BITS + CH_LOG2 + COUNT_BITS : i*ED_BITS + CH_LOG2 + 1];
-			assign dx = eds[i*ED_BITS + CH_LOG2 + COUNT_BITS + COUNT_BITS : i*ED_BITS + COUNT_BITS + CH_LOG2 + 1];
-			assign temp = temp ^ ({(CH_MAX){toggle}} 
-									 & ((x + dx * outer_count >= count) << ch_id));
+			ed_comp #(.IN_BITS(COUNT_BITS), .OUT_BITS(CH_MAX), .OUT_BITS_LOG2(CH_LOG2)) ec(
+							.count			(count),
+							.outer_count	(outer_count),
+							.x					(eds[(i * ED_BITS) + COUNT_BITS - 1 : (i * ED_BITS) + 0]),
+							.dx				(eds[(i * ED_BITS) + (2 * COUNT_BITS) - 1 : (i * ED_BITS) + COUNT_BITS]),
+							.ch_ID			(eds[((i + 1) * ED_BITS) - 2 : (i + 1) * ED_BITS - CH_LOG2 - 1]),
+							.enable			(eds[((i + 1) * ED_BITS) - 1]),
+							.out				(combo[(i+1) * CH_MAX - 1 : i * CH_MAX])
+							);
 		end 
-		assign state = state0 ^ temp;
 	endgenerate 
+	wire [CH_MAX - 1 : 0] temp; 
+	nXOR #(.width(CH_MAX), .n(ED_MAX)) nXOR0 (.in(combo), .out(temp));
+	assign  state = state0 ^ temp;
 	
 endmodule
